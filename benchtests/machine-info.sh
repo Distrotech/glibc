3eb83343274fda15b995727da7eedb9a5e67d7f5
Siddhesh Poyarekar <siddhesh@redhat.com>
2014-02-03 12:36:15 +0530

    Make bench.out in json format

--- benchtests/machine-info.sh
+++ benchtests/machine-info.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # Simple script to get some basic machine information
 
-{ sed -n -e 's/\(vendor_id\)[ \t]\+:/1. \1:/p' \
-	 -e 's/\(cpu family\)[ \t]\+:/2. \1:/p' \
-	 -e 's/\(model\)[ \t]\+:/3. \1:/p' \
-	 -e 's/\(cpu cores\)[ \t]\+:/4. \1:/p' \
-	 -e 's/\(cache size\)[ \t]\+:/6. \1:/p' \
-	 -e 's/MemTotal:[ \t]\+\(.*\)/7. memory: \1/p' \
-	 -e 's/SwapTotal:[ \t]\+\(.*\)/8. swap: \1/p' \
-	 /proc/cpuinfo /proc/meminfo; \
-	echo 5. processors: $(grep -c 'processor' /proc/cpuinfo); } | 
-	sort -u
+sed -n  -e 's/vendor_id[ \t]\+:\(.*\)/  "vendor-id": \1,/p' \
+	-e 's/cpu family[ \t]\+:\(.*\)/  "cpu-family": \1,/p' \
+	-e 's/\(model\)[ \t]\+:\(.*\)/  "\1": \1,/p' \
+	-e 's/cpu cores[ \t]\+:\(.*\)/  "cpu-cores": \1,/p' \
+	-e 's/cache size[ \t]\+:\([0-9]\+\).*/  "cache-size": \1,/p' \
+	-e 's/MemTotal:[ \t]\+\([0-9]\+\).*/  "memory": \1,/p' \
+	-e 's/SwapTotal:[ \t]\+\([0-9]\+\).*/  "swap": \1,/p' \
+	 /proc/cpuinfo /proc/meminfo | sort -u
+echo "  \"processors\": $(grep -c 'processor' /proc/cpuinfo),"
